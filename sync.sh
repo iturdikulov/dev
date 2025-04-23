64457759b94b4032e4e003630d90ac2dd555ce11
Inom Turdikulov <i@inomoz.ru>
2025-04-23 12:11:58 +0300

    Adjust sync script

--- sync.sh
+++ sync.sh
@@ -2,22 +2,26 @@
 
 # An rsync that respects gitignore
 rcp() {
-  #   -a = -rlptgoD
-  #   -r = recursive
-  #   -l = copy symlinks as symlinks
-  #   -p = preserve permissions
-  #   -t = preserve mtimes
-  #   -g = preserve owning group
-  #   -o = preserve owner
-  # -z = use compression
-  # -P = show progress on transferred file
-  # -J = don't touch mtimes on symlinks (always errors)
-  rsync -rtzPJ \
-    --include=.git/ \
-    "$@"
+    #   -a = -rlptgoD
+    #   -r = recursive
+    #   -l = copy symlinks as symlinks
+    #   -p = preserve permissions
+    #   -t = preserve mtimes
+    #   -g = preserve owning group
+    #   -o = preserve owner
+    # -z = use compression
+    # -P = show progress on transferred file
+    # -J = don't touch mtimes on symlinks (always errors)
+    rsync -rtzPJ \
+        --include=.git/ \
+        "$@"
 }
 
-BASE_PATH=/mnt/inom
+VIRTIO_NAME=inom
+VIRTIO_PATH=/mnt
+BASE_PATH="$VIRTIO_PATH/$VIRTIO_NAME"
+
+sudo mount -t virtiofs "$VIRTIO_NAME" "$VIRTIO_PATH" || echo "Already mounted"
 
 read -p "Sync files!"
 
@@ -25,3 +29,33 @@ rcp --delete $BASE_PATH/.ssh $HOME/
 rcp --delete $BASE_PATH/Wiki $HOME/
 rcp --delete $BASE_PATH/Life/ $HOME/Documents/Personal
 rcp --delete $BASE_PATH/.secrets/password-store/ ~/.password-store/
+
+FIREFOX_PROFILES="$HOME/.mozilla/firefox"
+FIREFOX_PROFILE_PATH="$FIREFOX_PROFILES/inom.default"
+
+mkdir -p "$FIREFOX_PROFILE_PATH"
+cat > "$FIREFOX_PROFILES/profiles.ini" << EOF
+[Profile0]
+Name=default-esr
+IsRelative=1
+Path=inom.default
+Default=1
+
+[General]
+StartWithLastProfile=1
+Version=2
+EOF
+
+FIREFOX_PROFILE_SRC="$BASE_PATH/.mozilla/firefox/inom.default"
+cd "$FIREFOX_PROFILE_SRC" || {
+    echo "Failed to change directory to $FIREFOX_PROFILE_SRC"
+    exit 1
+}
+for i in $(cat <<EOF
+cookies.sqlite
+places.sqlite
+permissions.sqlite
+EOF
+); do rcp "$FIREFOX_PROFILE_SRC/$i" "$FIREFOX_PROFILE_PATH"; done
+
+firefox -p
