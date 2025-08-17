f4e3e518ad0ba0feabfe842ab79dcdbccab7cc69
Inom T. <i@inomoz.ru>
2025-08-17 20:44:06 +0300

    Update global env, use more compatible env filename

---
+++ .config/plasma-workspace/env/env.sh
@@ -0,0 +1,3 @@
+export PATH=$HOME/.local/scrcpy:$HOME/.local/cling/bin:$HOME/.local/npm/bin:$HOME/.local/go/bin:$HOME/.local/bin:$HOME/.local/.npm-global/bin:$HOME/.config/nnn/plugins:$HOME/.local/scripts:$HOME/.local/tmux/bin:$HOME/.local/n/bin:/usr/local/go/bin:$HOME/.cargo/bin:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/games:$PATH
+export PARSEC_PEER_ID=30Q1Z8FJ1yXfXyThFiBR45gScuN
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
